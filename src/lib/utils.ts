dd1a3676f82ed54e7aa0fd9026afad05d44e4d43
rcoopr <rossadamcooper@gmail.com>
2025-01-27 10:19:27 +0000

    add shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function shuffle<T>(array: T[]): T[] {
   return array.sort(() => Math.random() - 0.5)
@@ -5,4 +7,8 @@ export function shuffle<T>(array: T[]): T[] {
 
 export function findMaxBy<T>(array: T[], fn: (item: T) => number): T {
   return array.reduce((max, item) => (fn(item) > fn(max) ? item : max), array[0])
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
